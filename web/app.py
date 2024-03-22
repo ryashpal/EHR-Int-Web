676a9d89d877889ba3c8c2dcb2ef91b21cfe879c
Yashpal Ramakrishnaiah <ryashpal89@gmail.com>
2024-03-22 06:37:00 +0000

    temp code to display patient list

--- web/app.py
+++ web/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+
 
 import logging
 logging.basicConfig(filename='logs/flask.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
@@ -15,7 +18,11 @@ def index():
 
 @app.route('/patient', methods = ['GET', 'POST'])
 def patient():
-    return render_template('patient.html', the_title="EHR-ML: Patients")
+    if request.method == 'POST':
+        patientsList = [{"Patient ID": "001", "Gender": "Male"}, {"Patient ID": "002", "Gender": "Female"}]
+        return render_template('patient.html', the_title="EHR-ML: Patients", patients=patientsList)
+    elif request.method == 'GET':
+        return render_template('patient.html', the_title="EHR-ML: Patients")
 
 
 if __name__ == "__main__":
